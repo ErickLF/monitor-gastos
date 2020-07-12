c276ada2adf41f4b46b9e20e50b1bbe676001590
Erick Fernando Lopez Fimbres <erick.lopez.fimbres@gmail.com>
2020-07-12 13:59:25 -0700

    [ADD] --> clase para crear las transacciones realizadas.

---
+++ monitor_app/lib/models/transaction.dart
@@ -0,0 +1,14 @@
+class Transaccion {
+  String id;
+  String titulo;
+  double precio;
+  DateTime fecha;
+
+  //Definimos el constructor de la clase por nombres
+  Transaccion({
+    this.id, 
+    this.titulo, 
+    this.precio,
+    this.fecha
+  });
+}
